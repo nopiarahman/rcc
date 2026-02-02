4192cb1fd2076506cda75560638073bed7755f59
nopiarahman <nopiarahman93@gmail.com>
2026-02-02 23:01:05 +0700

    web name

--- resources/views/welcome-screen.blade.php
+++ resources/views/welcome-screen.blade.php
@@ -54,7 +54,7 @@
     }, 5000);">
 <head>
     <meta charset="UTF-8">
-    <title>Selamat Datang - Raihaan Coffee Corner</title>
+    <title>Selamat Datang {{ $webSettings->name ?? 'Raihaan Coffee Corner' }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="//unpkg.com/alpinejs" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
